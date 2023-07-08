4af12369489a16b92f46bde47cb5a503c8008b6c
FacuMozo <facumozo98@gmail.com>
2023-07-08 20:18:21 -0300

    cambio de color de powerups

--- Assets/Code/Scripts/CircleBasic.cs
+++ Assets/Code/Scripts/CircleBasic.cs
@@ -2,22 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class NewBehaviourScript : MonoBehaviour
 {
     public string power;
 
     Vector3 speed = new Vector3(0.0f, -2.0f, 0.0f);
 
+    public bool inital = false;
+
+    Color[] colors = {Color.green,Color.red, Color.white, Color.blue, Color.yellow, Color.black}; 
+    string[] powerUps = {"rocket","heavy","slippery","water","rocks","dust" };
+
     // Start is called before the first frame update
     void Start()
     {
+        int index= Random.Range(0,colors.Length);
+        int moneda= Random.Range(0,2);
+       
         
+        // Accede al color aleatorio utilizando el índice generado
+        Color randomColor = colors[index];
+
+        // Accede al componente de material o color del objeto actual
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            // Aplica el color aleatorio al material del objeto
+            renderer.material.color = randomColor;
+        }
+        else
+        {
+            // Si no hay un componente Renderer, intenta acceder al componente de color
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            if (spriteRenderer != null)
+            {
+                // Aplica el color aleatorio al componente de color del objeto (sprite)
+                spriteRenderer.color = randomColor;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + speed * Time.deltaTime;
+        // transform.position = transform.position + speed * Time.deltaTime;
     }
 
     void OnTriggerEnter2D(Collider2D collision)

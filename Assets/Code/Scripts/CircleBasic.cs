ae013a58b3238d8d632c1ac29dc4b085b92b30b8
EzeCarbajo <carbajoezequiel@gmail.com>
2023-07-08 20:11:02 -0300

    logica de rocas

--- Assets/Code/Scripts/CircleBasic.cs
+++ Assets/Code/Scripts/CircleBasic.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public string power;
+    public GameObject rocks;
 
     Vector3 speed = new Vector3(0.0f, -2.0f, 0.0f);
 
@@ -35,6 +36,7 @@ public class NewBehaviourScript : MonoBehaviour
                 case ("water"):
                     break;
                 case ("rocks"):
+                    Instantiate(rocks,new Vector3(0,5.392388f, 0), transform.rotation);
                     break;
                 case ("dust"):
                     break;

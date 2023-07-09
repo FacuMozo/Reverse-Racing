9634ab95e06ad9bdc9365029964b1a587e2fd485
EzeCarbajo <carbajoezequiel@gmail.com>
2023-07-09 02:57:52 -0300

    Endgame?

--- Assets/Code/Scripts/GameManager.cs
+++ Assets/Code/Scripts/GameManager.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject gameOverCanvas;
     public GameObject youWinCanvas;
     int cantPlayers = 5;
+    public string sceneToLoad;
+    public string sceneToBack;
 
     // Start is called before the first frame update
     void Start()
@@ -35,5 +38,15 @@ public class GameManager : MonoBehaviour
         if(cantPlayers == 1){
             activateYouWin();
         }
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void exitGame()
+    {
+        SceneManager.LoadScene(sceneToBack);
     }
 }
